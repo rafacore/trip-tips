9a53cfacaa3b539f090549c4f5123562ee264fb2
rafael <rafanascimento.au@gmail.com>
2024-10-04 18:13:44 +1000

    config

--- client/app/api.js
+++ client/app/api.js
@@ -1,1 +1,2 @@
-export let tripsApi = `http://localhost:5000/api`;
+// export let tripsApi = `http://localhost:5000/api`;
+export let tripsApi = 'https://trip-tips-qj8nwounq-rafacores-projects.vercel.app/';
